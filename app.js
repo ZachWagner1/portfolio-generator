ced00c59d64579fc99608a940717f1eb4c209bb0
Zachary Wagner <zwag14@yahoo.com>
2024-02-29 17:53:40 -0600

    fixed exports

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 import inquirer from 'inquirer';
-// const fs = require('fs');
-// const generatePage = require('./src/page-template.js');
-
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile('index.html', generatePage(name, github), err => {
-//     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+import fs from 'fs';
+import pkg from './src/page-template.cjs';
+const {generatePage} = pkg;
 
 const promptUser = () => {
     return inquirer.prompt([
@@ -143,5 +136,11 @@ const promptProject = portfolioData => {
 promptUser()
     .then(promptProject)
     .then(portfolioData => {
-        console.log(portfolioData);
+        const pageHTML = pkg(portfolioData);
+
+        // fs.writeFile('index.html', generatePage(name, github), err => {
+        //     if (err) throw err;
+
+        //     console.log('Portfolio complete! Check out index.html to see the output!');
+        // });
     });
